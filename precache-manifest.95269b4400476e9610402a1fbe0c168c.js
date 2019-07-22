8f7b1c2abe7b48349fdd77920fc2ef813152041f
Giacomo Sorbi <g.sorbi@gmail.com>
2019-07-22 12:55:40 +0100

    Updates

---
+++ precache-manifest.95269b4400476e9610402a1fbe0c168c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6462b2a4e67466930a208a0a72e4b61",
+    "url": "/dynamic-form/index.html"
+  },
+  {
+    "revision": "61c021e0bcce05bddedb",
+    "url": "/dynamic-form/static/css/main.7aced40d.chunk.css"
+  },
+  {
+    "revision": "093ab73470f330cf359d",
+    "url": "/dynamic-form/static/js/2.26e8737c.chunk.js"
+  },
+  {
+    "revision": "61c021e0bcce05bddedb",
+    "url": "/dynamic-form/static/js/main.b01b0b64.chunk.js"
+  },
+  {
+    "revision": "a4deee4487039942ec0a",
+    "url": "/dynamic-form/static/js/runtime~main.2ba414b6.js"
+  }
+]);
